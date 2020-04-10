8e785349c119a865dd26a55daa5af0ba423f2fc3
dandumitriu33 <dandumitriu33@gmail.com>
2020-04-10 10:46:25 +0300

    Add empty class/interface files

---
+++ src/com/codecool/uml/overriding/AbstractProcess.java
@@ -0,0 +1,4 @@
+package com.codecool.uml.overriding;
+
+public abstract class AbstractProcess {
+}
---
+++ src/com/codecool/uml/overriding/Order.java
@@ -0,0 +1,4 @@
+package com.codecool.uml.overriding;
+
+public class Order implements Orderable {
+}
